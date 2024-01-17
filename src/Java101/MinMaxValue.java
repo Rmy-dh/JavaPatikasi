84156043f812dd18480e69365217485640863f0d
Rümeysa Küçüktaşdemir <rumeysa37373@gmail.com>
2024-01-17 21:08:41 +0300

    update min and max value

--- src/Java101/MinMaxValue.java
+++ src/Java101/MinMaxValue.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MinMaxValue {
     public static void main(String[] args) {
         //Kullanıcının istediği sayıda sayıyı kullanıcıdan  alıp bu sayıların en büyüğü ve en küçüğünü ekrana yazdırma.
-        int n,number,max=1,min=1;
+        int n,number,max=Integer.MIN_VALUE,min=Integer.MAX_VALUE;
         Scanner input=new Scanner(System.in);
         System.out.print("Kaç tane sayı gireceksiniz: ");
         n=input.nextInt();
